391abdd03087c5e2d197522b626fa6a61a701550
YuliyaHit <nata.lychko@mail.ru>
2021-11-21 15:37:18 +0300

    Update App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+let num = 30,
+    flag = false;
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+        <p style={{color: 'red'}}>
+          Hello World!
         </p>
 
-        <p>
-        Hello World!
+        <p style={{margin: 10}}>
+          Вывод переменной: {num}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p style={{margin:10}}>
+          Вывод числа: {23} <br />
+          Вывод суммы чисел: {23 + num} <br />
+        </p>
+
+        {flag && <p> Логические операции</p>}
+        <p style={{margin:10}}> undefined {undefined} , null  {null}, true  {true} и false {false} не будут выводиться в разметку</p>
+        
+        <p style={{margin:10}}>
+          Использование тернарника: {flag ? 'Flag is true' :  'Flag is false'}
+        </p>
       </header>
     </div>
   );
