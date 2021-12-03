9d2d41a6b61584a9e2bbbd6d73bf52b179a05d85
YuliyaHit <nata.lychko@mail.ru>
2021-12-03 14:14:15 +0300

    task- 16

--- src/App.js
+++ src/App.js
@@ -1,38 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
-import {count, length} from './number.js';
+import React from 'react';
+const element = (<div>Hello World!</div>);
 
-let num = 30,
-    flag = false;
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p style={{color: 'red'}}>
-          Hello World!
-        </p>
-
-        <p style={{margin: 10}}>
-          Вывод переменной: {num}
-        </p>
-
-        <p style={{margin:10}}>
-          Вывод числа: {23} <br />
-          Вывод суммы чисел: {23 + num} <br />
-          Вывод произведения модуля: {count*length}
-        </p>
-
-        {flag && <p> Логические операции </p>}
-        <p style={{margin:10}}> undefined {undefined} , null  {null}, true  {true} и false {false} не будут выводиться в разметку</p>
-        
-        <p style={{margin:10}}>
-          Использование тернарника: {flag ? 'Flag is true' :  'Flag is false'}
-        </p>
-      </header>
-    </div>
-  );
-}
-
-export default (App);
+export { element };
