861935c8ea521efeb732029a86d17efd07311e8a
YuliyaHit <nata.lychko@mail.ru>
2021-12-03 16:34:48 +0300

    task-18

---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ItemList from '../ItemList/ItemList';
+
+const App = () => (<div>
+  <h1>Список дел:</h1>
+  <ItemList />
+</div>);
+
+export default App;
---
+++ src/components/Item/Item.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Item = () => (<spa>Очень важное дело!</spa>);
+
+export default Item;
