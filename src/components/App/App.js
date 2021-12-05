2a96918c9970fc96870a251f7f1a3229cae1d9a0
YuliyaHit <nata.lychko@mail.ru>
2021-12-05 17:51:46 +0300

    add styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
+import './App.css';
 
 const App = () => {
 	const items = [
@@ -9,8 +11,9 @@ const App = () => {
 		{value: 'Выбрать всем подарки'}
 	];
 
-	return (<div>
-		  <h1>Список дел:</h1>
+	return (<div className = "wrap">
+		  <h1 className = "wrap__title">Список дел:</h1>
+		  <InputItem />
 		  <ItemList items = {items} />
 		  <Footer count={3} />
 		</div>
