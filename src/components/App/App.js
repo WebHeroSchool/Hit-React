f5419dc349269d397163905ba723cc4b253f9dbd
YuliyaHit <nata.lychko@mail.ru>
2021-12-05 16:05:54 +0300

    done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 
-const todoItem = 'Написать новое приложение';
+const App = () => {
+	const items = [
+		{value: 'Написать новое приложение'},
+		{value: 'Сходить в клуб'},
+		{value: 'Выбрать всем подарки'}
+	];
 
-const App = () => (<div>
-  <h1>Список дел:</h1>
-  <ItemList todoItem = {todoItem} />
-  <Footer count={3} />
-</div>);
+	return (<div>
+		  <h1>Список дел:</h1>
+		  <ItemList items = {items} />
+		  <Footer count={3} />
+		</div>
+	);
+};
 
 export default App;
