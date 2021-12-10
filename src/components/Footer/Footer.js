e00eca57323c490e40b0d9919763d361d80e3867
YuliyaHit <nata.lychko@mail.ru>
2021-12-10 16:30:51 +0300

    add styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { ButtonGroup } from '@mui/material';
+import { Button } from '@mui/material';
+import styles from'./Footer.module.css';
 
-const Footer = ({count}) => (<div>Всего нужно выполнить дел: {count} </div>);
+const Footer = ({count}) => (<div className = {styles.footer}>
+
+	<span className = {styles.footer__count}>Осталось: {count} </span>
+	
+	<div className = "filters">
+		<ButtonGroup variant="outlined" aria-label="outlined button group">
+			<Button sx={{ fontSize: 8, p: '5px', textTransform: 'none' }}> Все </Button>
+			<Button sx={{ fontSize: 8, p: '5px', textTransform: 'none' }}> Активные </Button>
+			<Button sx={{ fontSize: 8, p: '5px', textTransform: 'none' }}> Завершенные </Button>
+		</ButtonGroup>
+	</div>
+
+		<Button sx={{ fontSize: 8, p: '5px', textTransform: 'none' }} variant="contained">Удалить все</Button>
+
+
+	</div>);
 
 export default Footer;
